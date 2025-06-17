58fc0b40cc4f4992111f3bf0af3074f23934bc71
Chhayanka Dewangan <chhayankadewangan2004@gmail.com>
2025-06-17 18:25:08 +0530

    Project Completed

---
+++ server/routes/blogRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { addBlog, addComment, DeleteBlogById, generateContent, getAllBlogs, getBlogById, getBlogComments, togglePublish } from '../controllers/blogControllers.js';
+import upload from '../middleware/multer.js';
+import auth from '../middleware/auth.js';
+
+const blogRouter=express.Router();
+
+blogRouter.post('/add',upload.single('image'),auth , addBlog);
+blogRouter.get('/all',getAllBlogs);
+blogRouter.get('/:blogId',getBlogById);
+blogRouter.post('/delete', auth, DeleteBlogById);
+blogRouter.post('/toggle-publish', auth, togglePublish);
+blogRouter.post('/add-comment', addComment);
+blogRouter.post('/comments', getBlogComments);
+blogRouter.post('/generate', auth , generateContent);
+
+
+export default blogRouter;
